e2c2b35ca4972e254ae5a73c88cbafa9f454a40e
Tasha R Zuniga <Tashazun@gmail.com>
2018-05-03 16:16:18 -0700

    trying to fix my many personal flaws

--- test/e2e/users.test.js
+++ test/e2e/users.test.js
@@ -2,59 +2,57 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection, createAdminToken } = require('./db');
 
-describe('User API', () => {
 
-    before(() => dropCollection('tasks'));
-    before(() => dropCollection('fluffs'));
+describe.only('User API', () => {
+
+    before(() => dropCollection('squares'));
+    before(() => dropCollection('levels'));
     before(() => dropCollection('users'));
     
     let adminToken = '';
     before(() => createAdminToken().then(t => adminToken = t));
 
-    let token = '';
 
-    const fluffs = [
-        { desc: 'You arrive at a freeway.' },
-        { desc: 'There is a wide river here.' },
-        { desc: 'You find a tall dead tree.' }
-    ];
-
-    before(() => {
-        fluffs.forEach(obj => {
-            request.post('/api/fluffs')
-                .set('Authorization', adminToken)
-                .send(obj)
-                .then();
-        });
-    });
-
-    let task1 = {
-        number: 1,
-        startingDesc: 'You begin to feel hungry. Better find some food.',
-        requiredItem: { 
-            type: 'walnut',
-            itemDesc: 'You discover a walnut, but cannot crack the shell with your beak. You pick it up.'
+    let square1 = {
+        coords: {
+            x: 0,
+            y: 0
         },
-        endpoint: {
-            desc: 'You arrive at an intersection, crossed by telephone wires.',
-            unresolved: 'If you needed to crack something, this would be a good spot for it.',
-            resolved: 'You drop the walnut onto the street. A car rolls over it, cracking the shell, and you carefully swoop down and devour the food inside.'
-        }
+        squareDesc: 'Never was a figure so lovingly wrought in stone.'
     };
-
+    
     before(() => {
-        return request.post('/api/tasks')
+        return request.post('/api/squares')
             .set('Authorization', adminToken)
-            .send(task1)
-            .then();
+            .send(square1)
+            .then(({ body }) => {
+                square1 = body;
+            });
     });
+
+    let token = '';
 
     let user = {
         name: 'Master Blaster',
         password: 'bartertown',
     };
 
-    before(() => {
+    it('posts a level', () => {
+        let level = {
+            levelNum: 1,
+            squares: [{
+                squareId: square1._id
+            }]
+        };
+        return request.post('/api/levels')
+            .set('Authorization', adminToken)
+            .send(level)
+            .then(({ body }) => {
+                level = body;
+            });
+    });
+
+    it('saves user and assigns starting square', () => {
         return request.post('/api/auth/signup')
             .send(user)
             .then(({ body }) => {
@@ -63,77 +61,81 @@ describe('User API', () => {
             });
     });
 
-    it('adds an item to inventory', () => {
-        return request.post(`/api/users/${user.id}/inventory`)
-            .set('Authorization', token)
-            .send(task1.requiredItem)
-            .then(({ body }) => {
-                assert.deepEqual([task1.requiredItem.type], body.inventory);
-            });
-    });
 
-    it('gets inventory', () => {
-        return request.get(`/api/users/${user.id}/inventory`)
-            .set('Authorization', token)
-            .then(({ body }) => {
-                assert.deepEqual([task1.requiredItem.type], body.inventory);
-            });
-    });
 
-    it('gets an option (corresponding to one of 4 directions) and populates it with information', () => {
-        const direction = 'n';
-        return request.get(`/api/users/${user.id}/options/${direction}`)
-            .set('Authorization', token)
-            .then(({ body }) => {
-                assert.ok(body.action);
-                assert.ok(body.info);
-            });
-    });
 
-    it('deletes an item from inventory', () => {
-        return request.delete(`/api/users/${user.id}/inventory`)
-            .set('Authorization', token)
-            .then(({ body }) => {
-                assert.deepEqual([], body.inventory);
-            });
-    });
+
+    // it('adds an item to inventory', () => {
+    //     return request.post(`/api/users/${user.id}/inventory`)
+    //         .set('Authorization', token)
+    //         .send(task1.requiredItem)
+    //         .then(({ body }) => {
+    //             assert.deepEqual([task1.requiredItem.type], body.inventory);
+    //         });
+    // });
+
+    // it('gets inventory', () => {
+    //     return request.get(`/api/users/${user.id}/inventory`)
+    //         .set('Authorization', token)
+    //         .then(({ body }) => {
+    //             assert.deepEqual([task1.requiredItem.type], body.inventory);
+    //         });
+    // });
+
+    // it('gets an option (corresponding to one of 4 directions) and populates it with information', () => {
+    //     const direction = 'n';
+    //     return request.get(`/api/users/${user.id}/options/${direction}`)
+    //         .set('Authorization', token)
+    //         .then(({ body }) => {
+    //             assert.ok(body.action);
+    //             assert.ok(body.info);
+    //         });
+    // });
+
+    // it('deletes an item from inventory', () => {
+    //     return request.delete(`/api/users/${user.id}/inventory`)
+    //         .set('Authorization', token)
+    //         .then(({ body }) => {
+    //             assert.deepEqual([], body.inventory);
+    //         });
+    // });
     
-    it('gets a user\'s current task number', () => {
-        return request.get(`/api/users/${user.id}/level`)
-            .set('Authorization', token)
-            .then(({ body }) => {
-                assert.isNumber(body.level);
-            });
-    });
+    // it('gets a user\'s current task number', () => {
+    //     return request.get(`/api/users/${user.id}/level`)
+    //         .set('Authorization', token)
+    //         .then(({ body }) => {
+    //             assert.isNumber(body.level);
+    //         });
+    // });
 
-    it('updates a user\'s current task number', () => {
-        const task2 = {
-            number: 2,
-            startingDesc: 'Different desc.',
-            requiredItem: { 
-                type: 'something',
-                itemDesc: 'Different desc.'
-            },
-            endpoint: {
-                desc: 'Different desc.',
-                unresolved: 'Nope, not yet.',
-                resolved: 'Yay, you.'
-            }
-        };
+    // it('updates a user\'s current task number', () => {
+    //     const task2 = {
+    //         number: 2,
+    //         startingDesc: 'Different desc.',
+    //         requiredItem: { 
+    //             type: 'something',
+    //             itemDesc: 'Different desc.'
+    //         },
+    //         endpoint: {
+    //             desc: 'Different desc.',
+    //             unresolved: 'Nope, not yet.',
+    //             resolved: 'Yay, you.'
+    //         }
+    //     };
 
-        return request.post('/api/tasks')
-            .set('Authorization', adminToken)
-            .send(task2)
-            .then(({ body }) => {
-                task2._id = body._id;
-                return request.put(`/api/users/${user.id}/level`)
-                    .set('Authorization', token)
-                    .send({ level: 2 });
-            })
-            .then(({ body }) => {
-                assert.equal(body.currentTask, task2._id);
-                assert.equal(body._id, user.id);
-            });
-    });
+    //     return request.post('/api/tasks')
+    //         .set('Authorization', adminToken)
+    //         .send(task2)
+    //         .then(({ body }) => {
+    //             task2._id = body._id;
+    //             return request.put(`/api/users/${user.id}/level`)
+    //                 .set('Authorization', token)
+    //                 .send({ level: 2 });
+    //         })
+    //         .then(({ body }) => {
+    //             assert.equal(body.currentTask, task2._id);
+    //             assert.equal(body._id, user.id);
+    //         });
+    // });
 
 });
